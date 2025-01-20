71cef5404793e5632e9d420e58346d4d73a15a7a
Teodoro Poulson <teodoropoulson184@gmail.com>
2025-01-20 09:46:50 +0000

    Project 05

---
+++ Algoritmia/Project 05/Python.py
@@ -0,0 +1,17 @@
+'''
+      Programador...: Teodoro Poulson
+      Data..........: 20/01/2025
+      Observação....: Calcular a area de um retângulo
+'''
+
+
+
+def calcular_area_retangulo(comprimento: float, largura: float) -> float:
+    return comprimento * largura
+
+comprimento = float(input("Introduza o comprimento do retângulo: "))
+largura = float(input("Introdua a largura do retângulo: "))
+
+area = calcular_area_retangulo(comprimento, largura)
+
+print(f"A área do retângulo é: {area:.2f}")
